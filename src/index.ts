8bf50a0fe4624edfba80692fba760ded323d7c23
Sebastian Perez <spezuluaga@gmail.com>
2022-12-23 20:15:46 -0500

    code - basic swagger was added (API Documentation)

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,22 @@
 // ESModules = imports and exports
 // CommonJs = require -> Deprecated
-import express, { Request, Response } from 'express';
+import express from 'express';
 import diariesRoutes from './routes/diaries';
+
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from './openapi.json';
 
 const app = express();
 
 // Se utiliza el middleware cuya función es transformar el req.body a json
 app.use(express.json());
 
-// Pasar esta variable a una variable de entorno
-const PORT = 3000;
-
-app.get('/ping', (_req: Request, res: Response) => {
-  console.log('Someone pinged here!!');
-  res.send('You are in ping page');
-});
-
 // Use routes, reg
 app.use('/api/diaries', diariesRoutes);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
+// Pasar esta variable a una variable de entorno
+const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT} 🎄`);
 });
